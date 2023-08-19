1731091fa1e45ca787b9cfaacb358af172993355
Allan Araujo <allanrsa@gmail.com>
2023-08-19 15:37:12 +0000

    comitando arrays.

---
+++ src/main/java/com/example/AppArrays.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class AppArrays {
+    public static void main(String[] args) {
+       double vetor[] = new double[4];
+       vetor[0] = 0;
+       vetor[1] = 100;
+       vetor[2] = 200;
+       vetor[3] = 300;
+       System.out.println(Arrays.toString(vetor));
+
+       double matriz[][] = {{10,20,30}, {40,50,60}};
+       System.out.println(Arrays.toString(matriz[0]));
+       System.out.println(Arrays.toString(matriz[1]));
+
+
+       
+    }
+}
