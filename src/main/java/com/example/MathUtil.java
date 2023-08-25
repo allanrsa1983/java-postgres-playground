f616997ba1a0f57b1c0564e99c9215487a453628
Allan Araujo <allanrsa@gmail.com>
2023-08-25 12:53:49 +0000

    testes/junit/mdc.

--- src/main/java/com/example/MathUtil.java
+++ src/main/java/com/example/MathUtil.java
@@ -5,7 +5,20 @@ public class MathUtil {
 
 
     public static int calcularMDC(int a, int b) {
-        int retorno = 0;
+        int retorno = -1;
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        int maior = Math.max(a, b);
+        int menor = Math.min(a, b);
+
+        if (menor > 0 && (maior%menor == 0)) {
+            retorno = menor;
+        }
+        if (menor == 0) {
+            retorno = Math.abs(maior);
+        }
 
         return retorno;
     }
--- src/test/java/com/example/MathUtilTest.java
+++ src/test/java/com/example/MathUtilTest.java
@@ -6,12 +6,58 @@ import org.junit.jupiter.api.Test;
 
 public class MathUtilTest {
     @Test
-    void testCalcularMDC() {
+    void testCalcularMDC_P1() {
         int x = 6; 
         int y = 3; 
 
         int esperado = 3;
-        int obtido = MathUtil.calcularMDC(6,3);
+        int obtido = MathUtil.calcularMDC(x,y);
+
+        assertEquals(esperado, obtido);
+
+    }
+    @Test
+    void testCalcularMDC_P2() {
+        int x = 6; 
+        int y = 2; 
+
+        int esperado = 2;
+        int obtido = MathUtil.calcularMDC(x,y);
+
+        assertEquals(esperado, obtido);
+
+    }
+    @Test
+    void testCalcularMDC_P3() {
+        int x = 6; 
+        int y = 0; 
+
+        int esperado = 6;
+        int obtido = MathUtil.calcularMDC(x,y);
+
+        assertEquals(esperado, obtido);
+
+    }
+
+    @Test
+    void testCalcularMDC_P4() {
+        int x = 6; 
+        int y = 2; 
+
+        int esperado = MathUtil.calcularMDC(y, x);
+        int obtido = MathUtil.calcularMDC(x,y);
+
+        assertEquals(esperado, obtido);
+
+    }
+
+    @Test
+    void testCalcularMDC_P5() {
+        int x = 6; 
+        int y = 6; 
+
+        int esperado = MathUtil.calcularMDC(y, x);
+        int obtido = MathUtil.calcularMDC(x,y);
 
         assertEquals(esperado, obtido);
 
